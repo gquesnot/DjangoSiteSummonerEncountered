e791cfc896e01fb7470a99a80d32399bbe445e3b
gquesnot <gaqu1994@gmail.com>
2021-10-06 13:31:35 +0200

    fix

--- restart.sh
+++ restart.sh
@@ -1,4 +1,3 @@
 git pull
 pkill gunicorn
-python manage.py collectstatic
-gunicorn LolSummonerEncountered.wsgi --bind 0.0.0.0:8000 --workers 1 --timeout 600000 --graceful-timeout 600000 -D
+gunicorn LolSummonerEncountered.wsgi --bind 0.0.0.0:8000 --workers 1 -t 600 -D
